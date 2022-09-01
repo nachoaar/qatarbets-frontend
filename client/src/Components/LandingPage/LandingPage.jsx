55912765008f18edf3c8720867757aaef7f253bb
Maikyls <85658257+Maikyls@users.noreply.github.com>
2022-09-01 12:51:23 -0500

    se componentiza Landing

--- client/src/Components/LandingPage/LandingPage.jsx
+++ client/src/Components/LandingPage/LandingPage.jsx
@@ -2,7 +2,7 @@ import { Navbar } from "../Navbar/Navbar";
 import { SectionDos } from "./SectionDos";
 import { SectionUno } from "./SectionUno";
 
-export const Landing_Page = () => {
+export const LandingPage = () => {
   return (
     <div className="flex flex-col">
       <Navbar />
