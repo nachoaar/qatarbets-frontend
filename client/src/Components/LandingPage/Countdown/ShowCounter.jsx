f6eb61296ccc848b671e2b1420306f0745266181
Maikyls <85658257+Maikyls@users.noreply.github.com>
2022-09-02 17:11:18 -0500

    se arregla contador con nuenas variables de color y fuente

--- client/src/Components/LandingPage/Countdown/ShowCounter.jsx
+++ client/src/Components/LandingPage/Countdown/ShowCounter.jsx
@@ -4,7 +4,7 @@ import DateTimeDisplay from './DateTimeDisplay';
 
 export const ShowCounter = ({ days, hours, minutes, seconds }) => {
     return (
-        <div className="show-counter flex gap-x-2 text-mikeWhite uppercase text-xl font-sans">
+        <div className="show-counter flex gap-x-2 text-white uppercase text-xl font-titulo">
             <DateTimeDisplay value={days} type={'Days'} isDanger={days <= 3} />
             <DateTimeDisplay value={hours} type={'Hours'} isDanger={false} />
             <DateTimeDisplay value={minutes} type={'Mins'} isDanger={false} />
