31068c94c6238de829c7c9edff656c0ddc56f4e6
gastonfuentes <gastonnicolasfuentes@gmail.com>
2022-09-04 15:22:57 -0300

    se agrego efecto transform a las card

--- client/src/Components/Utils/CardLarge.jsx
+++ client/src/Components/Utils/CardLarge.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const CardLarge = (props) => {
   return (
     <button className="h-60 wid-auto md:w-4/12 p-4 ">
-      <div className="h-full  bg-rojo flex flex-col justify-between py-2 px-4 ease-out duration-300 hover:bg-rojosec rounded-tl-lg rounded-tr-2xl rounded-br-lg rounded-bl-2xl  font-parrafo">
+      <div className="h-full  bg-rojo flex flex-col justify-between py-2 px-4 ease-out duration-300 hover:bg-rojosec rounded-tl-lg rounded-tr-2xl rounded-br-lg rounded-bl-2xl  font-parrafo hover:-translate-y-3">
         <div className="bg-blanco">
           <h1 className="bg-rojosec text-center text-white">{props.date}</h1>
         </div>
