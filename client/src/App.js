62417a33384190e080377b39a6f97538af48bd80
faramir07 <malg20012002@gmail.com>
2022-09-03 22:17:56 -0500

    feat(FormLogin.jsx) add login finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import FormLogin from "./Components/Forms/FormLogin/FormLogin";
 import FormRestration from "./Components/Forms/FormRegistration/FormRegistration";
 import { Home } from "./Components/HomeComponent/Home";
 import { LandingPage } from "./Components/LandingPage/LandingPage";
@@ -10,6 +11,7 @@ function App() {
       <Route exact path="/" element={<LandingPage />} />
       <Route path="/home" element={<Home />} />
       <Route exact path="/register" element={<FormRestration />} />
+      <Route exact path="/login" element={<FormLogin />} />
     </Routes>
   );
 }
