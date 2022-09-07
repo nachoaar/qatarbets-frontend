07ba111a6c81b84c6c89e4c2ad2cfcae8b331024
gastonfuentes <gastonnicolasfuentes@gmail.com>
2022-09-07 18:52:02 -0300

    se modifico seccion dos de la landingPage

--- client/src/Components/Utils/CardLarge.jsx
+++ client/src/Components/Utils/CardLarge.jsx
@@ -5,7 +5,7 @@ export const CardLarge = (props) => {
 
   return (
     <Link to={`/detail/${props.id}`}>
-      <button className="transition-colors duration-700 hover:bg-rojo scroll-smooth mb-3 snap-center h-52 w-80 bg-rojosec rounded-tr-3xl rounded-bl-3xl rounded-md flex flex-col p-6">
+      <button className="transition-colors duration-700 hover:bg-rojo scroll-smooth mb-3 snap-center h-52 w-80 bg-rojosec rounded-tr-3xl rounded-bl-3xl rounded-md flex flex-col p-4">
         <div className="w-full h-10 bg-morado flex justify-center items-center">
           <p className="font-parrafo text-2xl text-white">2022-11-20</p>
         </div>
@@ -13,13 +13,13 @@ export const CardLarge = (props) => {
           <p className="font-fifa text-md text-white">FIFA WORLD CUP</p>
           <p className="font-parrafo text-white">{`Group Stage - ${props.groupId}`}</p>
         </div>
-        <div className="w-full h-16 flex bg-morado items-center justify-center px-1">
+        <div className="w-full h-16 flex bg-morado items-center justify-center px-2">
           <div className="text-white flex justify-start items-center w-full gap-1">
-            <div className="h-10 w-10 rounded border-2 border-white bg-rojo overflow-hidden flex items-center justify-center">
+            <div className="h-8 w-10 rounded border-2 border-white bg-rojo overflow-hidden flex items-center justify-center">
               <img
                 src={`https://countryflagsapi.com/png/${props.home_team}`}
                 alt=""
-                className="h-10 w-10"
+                className="h-8 w-10"
               />
             </div>
             <p className="font-parrafo">{props.home_team}</p>
@@ -27,11 +27,11 @@ export const CardLarge = (props) => {
           <p className="text-white w-10 h-10 flex items-center justify-center font-fifa">vs</p>
           <div className="text-white flex justify-end items-center w-full gap-1">
             <p className="font-parrafo">{props.away_team}</p>
-            <div className="h-10 w-10 rounded border-2 border-white bg-rojo overflow-hidden flex items-center justify-center">
+            <div className="h-8 w-10 rounded border-2 border-white bg-rojo overflow-hidden flex items-center justify-center">
               <img
                 src={`https://countryflagsapi.com/png/${props.away_team}`}
                 alt=""
-                className="h-10 w-10"
+                className="h-8 w-10"
               />
             </div>
           </div>
@@ -47,7 +47,7 @@ export const CardLarge = (props) => {
             <p>3.5</p>
           </div>
         </div>
-      </button>    
+      </button>
     </Link>
   );
 };
