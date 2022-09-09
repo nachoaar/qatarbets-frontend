c55eb108cffc16bff46428ab37aed6197a5c8986
Ignacio Fuentes <infuentes96@gmail.com>
2022-09-09 17:01:04 -0300

    arreglando bugs en dashboard

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,7 +3,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import teams from "./reducer/teamSlice";
 import fixture from "./reducer/fixtureSlice";
 import group from "./reducer/groupSlice";
-// import user from "./reducer/userSlice";
+import user from "./reducer/userSlice";
 import message  from "./reducer/messageSlice";
 import matchReducer from "./reducer/matchSlice";
 import players from "./reducer/playersSlice";
