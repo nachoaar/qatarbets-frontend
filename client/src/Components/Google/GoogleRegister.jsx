bdd3585fabd51781953ce408f3008d751a963437
matuxer <matias@biott.com.ar>
2022-09-15 22:53:35 -0300

    Added a Google register button for testing

---
+++ client/src/Components/Google/GoogleRegister.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+import { useNavigate } from "react-router-dom";
+
+function GoogleRegister() {
+  const navigate = useNavigate();
+  const [token, setToken] = useState(false);
+  const [user, setUser] = useState({});
+
+  const createOrGetUser = async (response, addUser) => {
+    const decoded = jwt_decode(response.credential);
+    const userInfo = {
+      pass: decoded.sub,
+      email: decoded.email,
+      name: decoded.name,
+      avatar: decoded.picture,
+    };
+    setUser(userInfo);
+    const {data} = await axios.post('http://localhost:3001/user/login', {
+      pass: userInfo.pass,
+      email: userInfo.email
+    });
+    console.log(data);
+    if (data.message === 'Usuario logueado con exito!') {
+      navigate('/home');
+    }else {
+      setToken(true)
+    }
+  };
+
+  let handleChange = async (e) => {
+    e.preventDefault();
+    setUser((prevState) => {
+      return {
+        ...prevState,
+        age: e.target.value,
+      };
+    });
+  };
+
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+    const { data } = await axios.post(
+      "http://localhost:3001/user/register",
+      user
+    );
+    console.log(data);
+  };
+  return (
+    <div>
+      {!token ? (
+        <GoogleLogin
+          onSuccess={(res) => {
+            createOrGetUser(res);
+          }}
+          onError={(err) => console.log(err)}
+        />
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <label>
+            Edad:
+            <input
+              name="edad"
+              placeholder="Edad aqui"
+              onChange={handleChange}
+              type="number"
+            />
+          </label>
+          <button type="submit">Confirmar</button>
+        </form>
+      )}
+    </div>
+  );
+}
+
+export default GoogleRegister;
