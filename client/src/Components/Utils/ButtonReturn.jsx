d8847aec828c9b689d4d05083a202a654dfc2c4b
gastonfuentes <gastonnicolasfuentes@gmail.com>
2022-09-17 13:40:30 -0300

    se creo componente button de retorno

---
+++ client/src/Components/Utils/ButtonReturn.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+export const ButtonReturn = ({title}) => {
+
+  const navigate = useNavigate()
+
+  const onClick = (e)=>{
+    e.preventDefault();
+    navigate(`/home`)
+  }
+
+
+  return (
+    <button
+      onClick={(e) => onClick(e)}
+      className="w-full transition duration-700 bg-gris hover:bg-rojo text-rojo hover:text-white font-parrafo p-2 active:bg-violet-700 focus:outline-none focus:ring focus:ring-rojosec focus:bg-rojo focus:text-white"
+    >
+      <div className="flex flex-row justify-between">
+        <div className="p-3 flex items-center">
+          <h1 className="font-fifa font-medium text-lg">{title}</h1>
+        </div>
+      </div>
+    </button>
+  );
+};
