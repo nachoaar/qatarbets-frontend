74e8af83dfca29f35f33de4ea3bc1d962d4be30a
JorgeCanale <jorgecanale636@yahoo.com>
2022-09-18 20:33:32 -0300

    boton para simular group stage

--- client/src/Components/User/UserBetCard.jsx/UserCard.jsx
+++ client/src/Components/User/UserBetCard.jsx/UserCard.jsx
@@ -1,8 +1,4 @@
 import React from 'react'
-import { useDispatch } from 'react-redux';
-import { useEffect} from 'react';
-import { matchId } from '../../../redux/actions/matchActions';
-import { useSelector } from 'react-redux';
 
 
 
