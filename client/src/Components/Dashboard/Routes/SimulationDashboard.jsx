e66074a7f5bf174faf23a505ff912fff9debf2a7
JorgeCanale <jorgecanale636@yahoo.com>
2022-09-18 01:30:03 -0300

    quite el disable de los botones de simulaciones en el dashboard

--- client/src/Components/Dashboard/Routes/SimulationDashboard.jsx
+++ client/src/Components/Dashboard/Routes/SimulationDashboard.jsx
@@ -49,10 +49,10 @@ export const Simulation = () =>{
                 <div className="w-full h-14 flex items-center gap-2">
                     <svg className="fill-gristexto transition duration-500 hover:fill-rojosec" xmlns="http://www.w3.org/2000/svg" height="20" width="20"><path d="m8.938 13 4.958-4.938L12.833 7l-3.895 3.875-1.771-1.75-1.063 1.063ZM4.5 17q-.625 0-1.062-.438Q3 16.125 3 15.5v-11q0-.625.438-1.062Q3.875 3 4.5 3h11q.625 0 1.062.438Q17 3.875 17 4.5v11q0 .625-.438 1.062Q16.125 17 15.5 17Zm0-1.5h11v-11h-11v11Zm0-11v11-11Z"/></svg>
                     <h2 className="font-titulodash font-bold text-gristexto text-lg">Simulaciones</h2>
-                    <button onClick={(e)=> handleSimulateAll(e)} value='cargar octavos' disabled={flag2 === true}>cargar octavos</button>
-                    <button onClick={(e)=> handleSimulateAll(e)} value='simular octavos' disabled={flag2 === false}>simular octavos</button>
-                    <button onClick={(e)=> handleSimulateAll(e)} value='simular cuartos' disabled={flag2 === false}>simular cuartos</button>
-                    <button onClick={(e)=> handleSimulateAll(e)} value='simular semifinal' disabled={flag2 === false}>simular semifinal</button>
+                    <button onClick={(e)=> handleSimulateAll(e)} value='cargar octavos' >cargar octavos</button>
+                    <button onClick={(e)=> handleSimulateAll(e)} value='simular octavos' >simular octavos</button>
+                    <button onClick={(e)=> handleSimulateAll(e)} value='simular cuartos' >simular cuartos</button>
+                    <button onClick={(e)=> handleSimulateAll(e)} value='simular semifinal' >simular semifinal</button>
                     <button onClick={(e)=> handleSimulateAll(e)} value='reiniciar' >reiniciar</button>
                  </div>
                 <div className="grid grid-cols-3 grid-rows-3 gap-5 "> 
