d174bd0925834094b3581bffff5097a81f9d45e7
Maikyls <85658257+Maikyls@users.noreply.github.com>
2022-09-22 02:32:30 -0500

    se agrega nosotros a footerdesde rama stripe xd

--- client/src/Components/AboutUs/nosotros.js
+++ client/src/Components/AboutUs/nosotros.js
@@ -9,49 +9,49 @@ import migue from '../../media/migue.png'
 
 let nosotros = [
   {
-    nombre: "Matias Montes",
+    nombre: "Matias M.",
     img: MMM,
     linkedin: "https://www.linkedin.com/in/matias-montes-866b42134/",
     github: "https://github.com/matiasmanuelmontes"
   },
   {
-    nombre: "Mike Monsalve",
+    nombre: "Mike M.",
     img: mike,
     linkedin: "https://www.linkedin.com/in/mike-monsalve-1ba9691b3/",
     github: "https://github.com/Maikyls"
   },
   {
-    nombre: "Facundo Alvarez",
+    nombre: "Facundo A.",
     img: facu,
     linkedin: "https://www.linkedin.com/in/facundo-alvarez-357b1123a/",
     github: "https://github.com/Faku0"
   },
   {
-    nombre: "Miguel Linares",
+    nombre: "Miguel L.",
     img: migue,
     linkedin: "https://www.linkedin.com/in/miguel-linades-gamez-5745b5149/",
     github: "https://github.com/faramir07"
   },
   {
-    nombre: "Benjamin Canale",
+    nombre: "Benjamin C.",
     img: benja,
     linkedin: "https://www.linkedin.com/in/benjamin-canale-8914b023b/",
     github: "https://github.com/JorgeCanale"
   },
   {
-    nombre: "Ignacio Fuentes",
+    nombre: "Ignacio F.",
     img: nacho,
     linkedin: "https://www.linkedin.com/in/ignacionfuentes/",
     github: "https://github.com/nachoaar"
   },
   {
-    nombre: "Gaston Fuentes",
+    nombre: "Gaston F.",
     img: gaston,
     linkedin: "https://www.linkedin.com/in/gastonnicolasfuentes/",
     github: "https://github.com/gastonfuentes"
   },
   {
-    nombre: "Matias Biott",
+    nombre: "Matias B.",
     img: matiB,
     linkedin: "https://www.linkedin.com/in/matuxer/",
     github: "https://github.com/matuxer"
